5b70bb16f032bbcb5af158e564ecea6e5e8f4b95
nicolekarakin <nicole.karakin@gmail.com>
2022-07-01 16:58:00 +0200

    git actions

--- src/main/java/org/nnn4eu/nfishe/Main.java
+++ src/main/java/org/nnn4eu/nfishe/Main.java
@@ -3,7 +3,7 @@ package org.nnn4eu.nfishe;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
         String pass=PassInput.input(5);
         System.out.println("pass: "+pass );
     }
